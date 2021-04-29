0a5adb0ea4209c5cb8a919ae61689808434e5da0
Angel <loaizadavila.angeldario@gmail.com>
2021-04-29 21:56:59 +0200

    cambios con auth añadidos

--- app/src/main/java/com/cm/blabapiso/MainActivity.java
+++ app/src/main/java/com/cm/blabapiso/MainActivity.java
@@ -2,15 +2,42 @@ package com.cm.blabapiso;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG= "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void checkCurrentUser(){
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        if(user != null){
+            //Completar el tratamiento del usuario
+        }
+    }
+
+    public void getUserProfile(){
+        FirebaseUser user= FirebaseAuth.getInstance().getCurrentUser();
+        if(user != null){
+            String name = user.getDisplayName();
+            String email= user.getEmail();
+            Uri photoUrl= user.getPhotoUrl();
+            //Verificar que el email es valido
+            boolean emailVerified= user.isEmailVerified();
+            String uid = user.getUid();
+        }
+
+    }
+
 
     //Comentario de prueba
 
